4bea6b7fa84ac282d053c7271ba1a3a2fd7792a8
ziko7 <agayev.zaur@gmail.com>
2023-03-19 09:13:48 -0400

    Zaur

---
+++ src/test/java/tests/TestPolicyZaur.java
@@ -0,0 +1,130 @@
+package tests;
+
+import Pages.PackagesInUrl;
+import Pages.Policies_Page;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import utils.ConfigReader;
+import utils.Driver;
+
+import java.time.Duration;
+import java.util.Iterator;
+import java.util.List;
+
+public class TestPolicyZaur extends TestBase {
+
+
+    @Test
+    public void policyWebAccessValidation() throws InterruptedException {
+
+        Policies_Page policypage = new Policies_Page();
+        Thread.sleep(3000);
+        policypage.webaccess.click();
+        Assert.assertTrue(Driver.getDriver().getTitle().contains("Accessibility Information | JetBlue"));
+        System.out.println("Web Access page is displayed");
+    }
+
+
+    @Test
+    public void policySafetyvalidation() throws InterruptedException {
+
+        Policies_Page policypage = new Policies_Page();
+        Thread.sleep(3000);
+        policypage.safety.click();
+        Assert.assertTrue(Driver.getDriver().getTitle().contains("Safety"));
+        System.out.println("Safety page is displayed");
+    }
+
+
+    @Test
+    public void policytTarmacValidation() throws InterruptedException {
+
+        Policies_Page policypage = new Policies_Page();
+        Thread.sleep(3000);
+        policypage.tarmac.click();
+        System.out.println(Driver.getDriver().getTitle());
+        Assert.assertTrue(Driver.getDriver().getTitle().contains("JetBlue | Airline Tickets, Flights & Airfare"));
+        System.out.println("Tarmac page is displayed");
+    }
+
+    @Test
+    public void policyContractofCarriageValidation() throws InterruptedException {
+        Policies_Page policypage = new Policies_Page();
+        Thread.sleep(3000);
+        policypage.carriage.click();
+
+        if (Driver.getDriver().getTitle().contains("JetBlue | Airline Tickets, Flights & Airfare: Book Direct - Official Site")) {
+            System.out.println(Driver.getDriver().getTitle());
+            System.out.println("Contract of Carriage page is displayed");
+
+        }
+    }
+
+    @Test
+    public void policyLegalvalidation() throws InterruptedException {
+
+        Policies_Page policypage = new Policies_Page();
+        Thread.sleep(3000);
+        policypage.legal.click();
+        Assert.assertTrue(Driver.getDriver().getTitle().contains("Legal"));
+        System.out.println("Legal page is displayed");
+    }
+
+    @Test
+    public void policyCustomerServicePlanValidation() throws InterruptedException {
+
+        Policies_Page policypage = new Policies_Page();
+        Thread.sleep(3000);
+        policypage.customer.click();
+        Assert.assertTrue(Driver.getDriver().getTitle().contains("Customer Service Plan"));
+        System.out.println("Customer Service Plan page is displayed");
+
+
+
+
+
+
+    }
+
+    @Test
+    public void policyPrivacyValidation() throws InterruptedException {
+
+        Policies_Page policypage = new Policies_Page();
+        Thread.sleep(3000);
+        policypage.privacy.click();
+        Assert.assertTrue(Driver.getDriver().getTitle().contains("Privacy"));
+        System.out.println("Privacy Policy page is displayed");
+    }
+
+
+
+    @Test
+    public void policyHumanTraffickingValidation() throws InterruptedException {
+
+        Policies_Page policypage = new Policies_Page();
+        Thread.sleep(3000);
+        policypage.humantrafficking.click();
+        if (Driver.getDriver().getTitle().contains("Human Trafficking"));
+        System.out.println("Human Trafficking page is displayed");
+    }
+
+    @Test
+    public void policyOptionalServicesValidation() throws InterruptedException {
+
+        Policies_Page policypage = new Policies_Page();
+        Thread.sleep(3000);
+        policypage.optional.click();
+        if (Driver.getDriver().getTitle().contains("Optional Services and Fees"));
+        System.out.println("Optional Services and Fees page is displayed");
+    }
+
+
+
+
+}
+
+
