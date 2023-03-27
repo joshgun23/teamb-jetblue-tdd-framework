dc8e62d39838f083ab4b1bc40212be789c01ce75
Renata Saudargaite <rsaudar@gmail.com>
2023-03-27 05:40:10 -0400

    2nd sprint - Renata

--- src/test/java/Tests_Sprint_2/BestFareFinderTest.java
+++ src/test/java/Tests_Sprint_2/BestFareFinderTest.java
@@ -1,24 +1,83 @@
 package Tests_Sprint_2;
 
-import org.openqa.selenium.By;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import pages_Sprint_2.BestFareFinderPage;
 import tests.TestBase2;
 import utils.Driver;
+import utils.SeleniumUtils;
 
 
 public class BestFareFinderTest extends TestBase2 {
 
     @Test
-    public void fareFinderButtonTest() throws InterruptedException {
-
+    public void fareFinder ()  {
         BestFareFinderPage bestFareFinderPage = new BestFareFinderPage();
         bestFareFinderPage.clickOnBook();
         bestFareFinderPage.clickOnBestFareFinder();
         Assert.assertEquals(Driver.getDriver().getTitle(),"Best Fare Finder | JetBlue");
     }
+    @Test (dataProvider = "airportCodeProvider")
+    public void testFlightFrom (String airportCode) {
+        BestFareFinderPage bestFareFinderPage = new BestFareFinderPage();
+        bestFareFinderPage.clickOnBook();
+        bestFareFinderPage.clickOnBestFareFinder();
+        new BestFareFinderPage().flightFrom(airportCode);
+        Assert.assertTrue(Driver.getDriver().getPageSource().contains(airportCode));
+    }
+    @Test (dataProvider = "airportCodeProvider")
+    public void testFlightTo (String airportCode) {
+        BestFareFinderPage bestFareFinderPage = new BestFareFinderPage();
+        bestFareFinderPage.clickOnBook();
+        bestFareFinderPage.clickOnBestFareFinder();
+        new BestFareFinderPage().flightTo(airportCode);
+        Assert.assertTrue(Driver.getDriver().getPageSource().contains(airportCode));
 
-    @Test
-    public void
+    }
+    @DataProvider
+    public Object[][] airportCodeProvider() {
+        return SeleniumUtils.readFromCSV("DataAirport.csv");
+    }
+
+    @Test (dataProvider = "airportCode")
+        public void testFlightWithSameFromAndTo (String airportCode1, String airportCode2) {
+        BestFareFinderPage bestFareFinderPage = new BestFareFinderPage();
+        bestFareFinderPage.clickOnBook();
+        bestFareFinderPage.clickOnBestFareFinder();
+        new BestFareFinderPage().flightFrom(airportCode1);
+        new BestFareFinderPage().flightTo(airportCode2);
+        Assert.assertEquals(bestFareFinderPage.getErrorMessage().getText(), "Uh-oh. It doesn't look like we fly between those two locations. Please try another city nearby." );
+    }
+
+    @DataProvider
+    public Object[][] airportCode() {
+        return new Object[][] {
+                {"LAX", "LAX"}
+        } ;
+    }
+
+    @Test (dataProvider = "destination")
+    public void testExploreFares (String city1, String city2) {
+        BestFareFinderPage bestFareFinderPage = new BestFareFinderPage();
+        bestFareFinderPage.clickOnBook();
+        bestFareFinderPage.clickOnBestFareFinder();
+        new BestFareFinderPage().flightFrom(city1);
+        new BestFareFinderPage().flightToExplore(city2);
+        new BestFareFinderPage().clickOnExploreFares();
+        Assert.assertTrue(bestFareFinderPage.getDepartureDate().isDisplayed());
+    }
+    @DataProvider
+    public Object[][] destination() {
+        return new Object[][] {
+                {"SFO", "CDG"}
+        } ;
+    }
+
+
+
+
 }
+
+
+
--- src/test/java/pages_Sprint_2/BestFareFinderPage.java
+++ src/test/java/pages_Sprint_2/BestFareFinderPage.java
@@ -1,15 +1,25 @@
 package pages_Sprint_2;
 
-import org.openqa.selenium.By;
+import com.github.javafaker.Faker;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import utils.Driver;
+import utils.SeleniumUtils;
+
+import java.time.Duration;
 
 public class BestFareFinderPage {
 
+    private WebDriverWait wait;
+
     public BestFareFinderPage () {
         PageFactory.initElements(Driver.getDriver(), this);
+        wait = new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(15));
+
     }
 
 // Locators
@@ -20,6 +30,22 @@ public class BestFareFinderPage {
     @FindBy(linkText = "Best Fare Finder")
     private WebElement bestFareFinder;
 
+    @FindBy(id = "jb-autocomplete-0-search")
+    private WebElement fromInputField;
+
+    @FindBy(id = "jb-autocomplete-1-search")
+    private WebElement toInputField;
+
+    @FindBy(xpath = "//span[normalize-space()='Explore fares']")
+    private WebElement exploreFaresButton;
+
+    @FindBy(xpath = "//p[@role='alert']")
+    private WebElement errorMessage;
+
+    @FindBy(xpath = "//div[normalize-space()='Select depart date']")
+    private WebElement departureDate;
+
+
 
 // Return methods
 
@@ -29,9 +55,42 @@ public class BestFareFinderPage {
 
     public WebElement getBestFareFinder() { return bestFareFinder; }
 
-    public void clickOnBestFareFinder () { bestFareFinder.click();
+    public void clickOnBestFareFinder () { bestFareFinder.click(); }
+
+    public WebElement getExploreFaresButton() { return exploreFaresButton; }
+
+    public void clickOnExploreFares() { exploreFaresButton.click();}
+
+    public WebElement getFromInputField() { return fromInputField; }
+
+    public WebElement getErrorMessage() { return errorMessage; }
+
+    public WebElement getDepartureDate() {return departureDate; }
+
+    public WebElement getToInputField() { return toInputField; }
+
+
+    public void flightFrom(String from) {
+        wait.until(ExpectedConditions.elementToBeClickable(this.fromInputField)).click();
+        fromInputField.click();
+        fromInputField.clear();
+        fromInputField.sendKeys(from);
+        fromInputField.sendKeys(Keys.ENTER);
     }
 
 
+    public void flightTo(String to) {
+        wait.until(ExpectedConditions.elementToBeClickable(this.toInputField)).click();
+        toInputField.click();
+        toInputField.clear();
+        toInputField.sendKeys(to);
+    }
 
+    public void flightToExplore(String city) {
+        wait.until(ExpectedConditions.elementToBeClickable(this.toInputField)).click();
+        toInputField.click();
+        toInputField.clear();
+        toInputField.sendKeys(city);
+        toInputField.sendKeys(Keys.ENTER);
+    }
 }
